4979ed8d82d4f5f2e2568233bd79df285bc5671b
Lucas de Vargas Souza <lvargas@gvdasa.local>
2019-08-12 13:25:04 -0300

    Avanços na criação da create gifs table

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -91,6 +91,20 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _createGifsTable(BuildContext context, AsyncSnapshot snapshot) {
-    return Container();
+    return GridView.builder(
+      padding: EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+      itemCount: snapshot.data["data"].lenght,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Image.network(
+            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
+    );
   }
 }
