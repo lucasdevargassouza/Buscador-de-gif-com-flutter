198315f9b6869794120aff75f64b4431cac4e459
Lucas DeVargas Souza <ls18017@gmail.com>
2019-08-11 22:50:35 -0300

    App criado e modificado. Concluído até a metade do curso.

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:buscador_gifs/pages/home_page.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    title: "Buscador de gifs",
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
+
+
+
+
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _search;
+  int _offset;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=7L3UFbJtYx4JSalTIErneH8rA6peKNIH&limit=20");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=7L3UFbJtYx4JSalTIErneH8rA6peKNIH&q=$_search&offset=$_offset&limit=20&rating=g");
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) {
+      print(map);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Pesquizar aqui...",
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(),
+              ),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError)
+                      return Container();
+                    else
+                      return _createGifsTable(context, snapshot);
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _createGifsTable(BuildContext context, AsyncSnapshot snapshot) {
+    return Container();
+  }
+}
